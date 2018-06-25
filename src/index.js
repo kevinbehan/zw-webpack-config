3fab62c91bb658fd4ad4f008b362957b542394a2
Kevin Behan <kevinbehan2017@gmail.com>
2018-06-25 21:56:05 +0000

    Add loader for css/scss, import scss to ensure it works correctly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//Test styles to make sure multiple .scss files are concatenated into one .css file
+import './index.scss'
+
 console.log('Hello friends at Zillow! This is my little test file for my build process.')
 
 //Using code below to make sure ES6 features are transpiled to ES5
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -18,10 +18,22 @@ module.exports = {
                 options: {
                     presets: ['env']
                 }
+            },
+            {
+                test: /\.scss?/,
+                loader: ExtractTextPlugin.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
             }
         ]
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: 'style.[chunkhash:8].css'
+        }),
         new CleanWebpackPlugin(
             ['build/**/*.*'],
             { root: path.resolve('.') }
